d038c3e93bd74eeab03a5bce678acb7197d2b6b3
Sanjeev <sanjeevkansyakar789@gmail.com>
2023-10-27 20:48:01 +0530

    chnage nav

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -1,9 +1,12 @@
 import DashboardNav from "@/components/navbar/DashboardNav";
+import { Suspense } from "react";
 
 const MainLayout = ({ children }) => {
   return (
     <main>
-      <DashboardNav />
+      <Suspense fallback={<h2>Loading Nav</h2>}>
+        <DashboardNav />
+      </Suspense>
 
       <div className="pt-10">{children}</div>
     </main>
