2ef79e379c3bd02beaf184415e6c9651d745db61
Sanjeev <sanjeevkansyakar789@gmail.com>
2023-10-28 17:45:23 +0530

    dashboard change

--- src/components/editor/DashboardContent.jsx
+++ src/components/editor/DashboardContent.jsx
@@ -10,35 +10,35 @@ import Input from "@/components/Input";
 import TipTapEditor from "@/components/editor/TipTapEditor";
 
 const DashboardContent = ({ blog }) => {
-  const { edgestore } = useEdgeStore();
-  const [isUploading, setIsUploading] = useState(false);
+  // const { edgestore } = useEdgeStore();
+  // const [isUploading, setIsUploading] = useState(false);
   // Required data for Submission
   const [title, setTitle] = useState(blog?.title || "");
   // File uploaded by user
-  const [file, setFile] = useState("");
+  // const [file, setFile] = useState('');
   // To show progressbar
-  const [progress, setProgress] = useState(0);
+  // const [progress, setProgress] = useState(0);
   // For Image
   const [imageUrl, setImageUrl] = useState(blog?.imageUrl || "");
 
-  const uploadToImageStore = async () => {
-    if (!file) return;
-    setIsUploading(true);
-    const res = await edgestore.publicFiles.upload({
-      file,
-      input: { type: "blog" },
-      onProgressChange: (progress) => {
-        setProgress(progress);
-      },
-    });
-    // to add the necessary data to your database
-    setImageUrl(res?.url);
-    setIsUploading(false);
-  };
+  // const uploadToImageStore = async () => {
+  //   if (!file) return;
+  //   setIsUploading(true);
+  //   const res = await edgestore.publicFiles.upload({
+  //     file,
+  //     input: { type: "blog" },
+  //     onProgressChange: (progress) => {
+  //       setProgress(progress);
+  //     },
+  //   });
+  //   // to add the necessary data to your database
+  //   setImageUrl(res?.url);
+  //   setIsUploading(false);
+  // };
 
   return (
     <div className="w-full flex max-md:flex-col gap-5 max-md:px-3">
-      {/* <Toaster /> */}
+      <Toaster />
 
       <div className="md:w-1/3 flex flex-col gap-5">
         <div className="h-fit md:backdrop-blur-lg bg-white/40 shadow-lg shadow-slate-300 rounded-lg p-2">
@@ -53,7 +53,7 @@ const DashboardContent = ({ blog }) => {
           <p className="w-full mb-1 p-1 text-lg font-semibold tracking-wide">
             Add an Image
           </p>
-          <SingleImageDropzone
+          {/* <SingleImageDropzone
             height={200}
             value={file}
             onChange={(file) => {
@@ -72,9 +72,9 @@ const DashboardContent = ({ blog }) => {
             {progress != 0 && (
               <span className="text-black text-sm">{progress}%</span>
             )}
-          </div>
+          </div> */}
 
-          <div className="flex justify-center items-center">
+          {/* <div className="flex justify-center items-center">
             <button
               disabled={isUploading}
               className=" focus-visible:ring-ring ring-1 ring-white transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 px-4 py-1.5 bg-black/90 text-white text-semibold rounded-xl hover:bg-black/70"
@@ -82,22 +82,22 @@ const DashboardContent = ({ blog }) => {
             >
               {isUploading ? "Uploading.." : "Upload"}
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
       <div className="flex md:w-2/3">
         <div className="flex w-full h-fit md:backdrop-blur-lg bg-white/40 shadow-lg shadow-slate-300 rounded-lg p-2">
           hello
-          {/* <TipTapEditor
+          <TipTapEditor
             title={title}
             setTitle={setTitle}
             imageUrl={imageUrl}
             setImageUrl={setImageUrl}
-            setProgress={setProgress}
-            setFile={setFile}
+            // setProgress={setProgress}
+            // setFile={setFile}
             content={blog?.content}
             blogId={blog?._id}
-          /> */}
+          />
         </div>
       </div>
     </div>
